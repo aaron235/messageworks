70fdabb9291efe4037ac6cadaa0dbf72fda49045
aaron235 <qwertyman159@gmail.com>
2014-09-12 13:19:34 -0700

    improved logging and (attempted to) make tasks run in background

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(mongod --dbpath db --smallfiles --logpath log/mongodb.log) &
-(morbo -v server.pl) &
+mongod --dbpath db --smallfiles --logpath log/mongodb.log >> log/mongod.log &
+morbo -l http://*:3000 server.pl >> log/morbo.log &
