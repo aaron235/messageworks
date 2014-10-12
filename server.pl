bc774ce57c9baee16d905fa19b417734b9901fac
aaron235 <qwertyman159@gmail.com>
2014-10-11 17:37:29 -0700

    Added option to join room that was requested, but already exists

--- server.pl
+++ server.pl
@@ -79,7 +79,7 @@ get '/' => sub {
 
 get '/about' => sub {
 	my $controller = shift;
-	
+
 	$controller->stash(
 		page => $controller->render_to_string( 'pages/about' ),
 		title => "About",
@@ -89,7 +89,7 @@ get '/about' => sub {
 
 get '/new' => sub {
 	my $controller = shift;
-	
+
 	$controller->stash(
 		page => $controller->render_to_string( 'pages/new' ),
 		title => "Create a New Room",
@@ -99,28 +99,29 @@ get '/new' => sub {
 
 post '/create' => sub {
 	my $controller = shift;
-	
+
 	## $private is a boolean, $requestedURL only exists if private is 0
 	my $private = int( $controller->param( 'private' ) );
 	my $requestedURL = $controller->param( 'roomURLInput' );
-	
+
 	if ((not defined($requestedURL)) || ($requestedURL eq "") || ($private == 1)) {
 		$requestedURL = generateSecureURL();
 	};
-	
+
 	if (!($requestedURL =~ m/[a-zA-Z][\w]{0,31}/ )) {
 		debugLog( "Regex failed, invalid name\n" );
 		$controller->redirect_to( '/new' );
 		return;
 	} elsif ( $requestedURL ~~ %rooms ) {
+		$controller->flash( name => $requestedURL );
 		$controller->redirect_to( 'error/room_already_exists' );
 	} else {
 		my $newRoom = addRoom( $requestedURL, $private );
 		$controller->redirect_to( 'chat/' . $newRoom->{id} );
 	};
-	
+
 	sub generateSecureURL {
-		my @chars = ( 
+		my @chars = (
 			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 			'0', '1' , '2', '3', '4', '5', '6', '7', '8', '9'
@@ -129,21 +130,21 @@ post '/create' => sub {
 		foreach ( 1 .. 31 ) {
 			$string .= $chars[ int( rand( @chars ) ) ];
 		};
-		
+
 		return $string;
 	};
-	
+
 	sub addRoom {
 		my $roomName = shift;
 		my $private = shift;
-		
+
 		my $room = Rooms->new(
 			id			=> $roomName,
 			private		=> $private,
 		);
 		##	Adds this room to the global hash of all open rooms
 		$rooms{$roomName} = $room;
-		
+
 		return $room;
 	};
 };
@@ -156,13 +157,13 @@ get '/chat/' => sub {
 get '/chat/:roomName' => sub {
 	my $controller = shift;
 	my $roomQuery = $controller->stash( 'roomName' );
-	
+
 	if ( $roomQuery ~~ %rooms ) {
 		$controller->stash(
 			page => $controller->render_to_string( 'pages/chat', title => $roomQuery ),
 			title => $roomQuery,
 		);
-		
+
 		$controller->render( 'frame' );
 	} else {
 		$controller->redirect_to( '/error/room_not_found' );
@@ -172,39 +173,41 @@ get '/chat/:roomName' => sub {
 get '/error/:error' => sub {
 	my $controller = shift;
 	my $error = $controller->stash( 'error' );
-	
+
 	my $title = join ( " ", split( "_", $error) );
-	
+
 	$title =~ s/(\w+)/\u\L$1/g;
-	
+
+	print( "#####" . $controller->flash( 'name' ) . "\n" );
+
 	$controller->stash (
-		page => $controller->render_to_string( 'errors/' . $error ),
+		page => $controller->render_to_string( 'errors/' . $error, name => $controller->flash( 'name' ) ),
 		title => "Error: " . $title,
 	);
-	
+
 	$controller->render( 'frame' );
 };
 
 websocket '/chat/:roomName/send' => sub {
 	my $controller = shift;
 	my $roomName = $controller->stash( 'roomName' );
-	
+
 	my $room = $rooms{$roomName};
-	
+
 	my $user = Users->new(
 		controller	=> $controller,
 	);
-	
+
 	$rooms{$roomName}->addUser($user);
-	
+
 	$rooms{$roomName}->serverMessage("Client " . $user->{rand} . " has connected.", "userConnect");
-	
+
 	##	set the timeout for each websocket connection to indefinite
 	$user->{controller}->inactivity_timeout( 0 );
 
 	$user->{controller}->on( json => sub {
 		my ($controller, $hashIn) = @_;
-		
+
 		given( $hashIn->{type} ) {
 			when( "message" ) {
 				my $hash = $user->signMessage( $hashIn );
@@ -214,15 +217,15 @@ websocket '/chat/:roomName/send' => sub {
 			when ( "name" ) {
 				$user->setName( $hashIn->{name} );
 				$room->sendUserList;
-			} 
+			}
 			when ( "backlog" ) {
 				$room->sendBacklog( $user, $hashIn->{amount} );
 			}
 			when ( "keepalive" ) {
-				
+
 			}
 			default {
-				
+
 			};
 		};
 	});
@@ -231,19 +234,19 @@ websocket '/chat/:roomName/send' => sub {
 	$user->{controller}->on( finish => sub {
 		##Write it down in the log, console, and room chat
 		app->log->debug( "Client disconnected" );
-		
+
 		#Remove the user
 		$rooms{$roomName}->removeUser($user);
-	
+
 		$room->serverMessage("Client " . $user->{rand} . " has disconnected.", "userDisconnect");
-		
+
 		## If the room is empty, delete the room.
 		if ( !keys $room->{clients} && $roomName ne "default" ) {
 			delete( $rooms{$roomName} );
 			$room->remove;
 		};
 	});
-}; 
+};
 
 
 ######  FUNCTIONS  ######
